1e09ab0aa15c2a721468349e06e9c74f93ada4d0
yogaxv <yoga.xv@gmail.com>
2021-03-18 11:35:35 +0700

    Add try catch to deployment

--- app/Http/Controllers/DeployController.php
+++ app/Http/Controllers/DeployController.php
@@ -4,22 +4,33 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Symfony\Component\Process\Process;
+use Exception, Log;
 
 class DeployController extends Controller
 {
     public function deploy(Request $request)
 	{
-	     $githubPayload = $request->getContent();
-	     $githubHash = $request->header('X-Hub-Signature');
-	     $localToken = config('app.deploy_secret');
+		try {
+			$githubPayload = $request->getContent();
+		    $githubHash = $request->header('X-Hub-Signature');
+		    $localToken = config('app.deploy_secret');
+
+		    $localHash = 'sha1=' . hash_hmac('sha1', $githubPayload, $localToken, false);
+
+		    Log::info("Github hash: $githubHash, LocalHas: $localHash");
+
+		    if (hash_equals($githubHash, $localHash)) {
+		        $root_path = base_path();
+		        $process = new Process('cd ' . $root_path . '; ./deploy.sh');
+		        $process->run(function ($type, $buffer) {
+		             echo $buffer;
+		        });
+		    }
+		} catch (Exception $e) {
+			
+			Log::error($e->getMessage().' in '.$e->getFile().' at '.$e->getLine());
+            return response()->json(['message' => $e->getMessage() ], $e->getCode() >  300 ?  $e->getCode() : 500 );
+		}
 	     
-	     $localHash = 'sha1=' . hash_hmac('sha1', $githubPayload, $localToken, false);
-	     if (hash_equals($githubHash, $localHash)) {
-	          $root_path = base_path();
-	          $process = new Process('cd ' . $root_path . '; ./deploy.sh');
-	          $process->run(function ($type, $buffer) {
-	              echo $buffer;
-	          });
-	     }
 	}
 }
